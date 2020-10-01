cc8f863a8cd70be260e117b881916fb14094a7c2
Oliver Lorenz <mail@oliverlorenz.com>
2020-10-01 13:57:09 +0200

    chore: wip

--- src/index.ts
+++ src/index.ts
@@ -1,60 +1,82 @@
 // @ts-ignore
 import * as DiscordWebhook from "discord-webhook-node";
+import { Webhook } from "discord-webhook-node";
 import { DiscordMessagingHandler } from "./discordMessagingHandler";
-import { VoiceChannelObserver } from "@gluecode-it/discord-voice-channel-observer";
+import {
+  VoiceChannelObserver,
+  Event,
+} from "@gluecode-it/discord-voice-channel-observer";
 import { DelayedTransitionHandler } from "@gluecode-it/delayed-transition-handler";
-import { VmHandler } from "@gluecode-it/google-cloud-vm-handler"
+import { VmHandler, GoogleVM } from "@gluecode-it/google-cloud-vm-handler";
+import { Client } from "discord.js";
 import { State } from "@gluecode-it/delayed-transition-handler/dist/src/state";
 
+const Compute = require("@google-cloud/compute");
+
 export class StartStopServerByDiscordVoiceChannel {
+  private startToStopTransitionHandler: DelayedTransitionHandler;
+  private stopToStartTransitionHandler: DelayedTransitionHandler;
+
   constructor(
-    private threshold: number, 
-    private delayStartupMs: number,
-    private delayShutdownMs: number,
     private observer: VoiceChannelObserver,
     private vmHandler: VmHandler,
     private messageHandler: DiscordMessagingHandler,
-  ) {}
+    private delayStartupMs: number,
+    private delayShutdownMs: number,
+    private startUpThreshold: number,
+    private shutdownUpThreshold: number
+  ) {
+    this.startToStopTransitionHandler = new DelayedTransitionHandler(
+      this.delayStartupMs
+    );
+    this.stopToStartTransitionHandler = new DelayedTransitionHandler(
+      this.delayShutdownMs
+    );
+  }
+
+  private async thresholdReachedHandler() {
+    this.startToStopTransitionHandler.onTransitionScheduled(async () => {
+      await this.messageHandler.sendStartScheduledMessage(this.delayStartupMs);
+    });
+
+    this.startToStopTransitionHandler.onTransitionStarted(async () => {
+      await this.messageHandler.sendStartingMessage();
+      //await vmHandler.start();
+    });
+
+    if (this.startToStopTransitionHandler.is(State.STATUS_A)) {
+      this.startToStopTransitionHandler.scheduleTransition();
+    }
+  }
+
+  private async thresholdLeftHandler() {
+    this.stopToStartTransitionHandler.onTransitionScheduled(async () => {
+      await this.messageHandler.sendStopScheduledMessage(this.delayShutdownMs);
+    });
+
+    this.stopToStartTransitionHandler.onTransitionStarted(async () => {
+      await this.messageHandler.sendStopMessage();
+      //await vmHandler.stop();
+    });
+
+    if (this.stopToStartTransitionHandler.is(State.STATUS_A)) {
+      this.stopToStartTransitionHandler.scheduleTransition();
+    }
+  }
+
   async start() {
     this.vmHandler.onStarted((ip) => {
-      this.messageHandler.sendRunningMessage(ip)
-    })
+      this.messageHandler.sendRunningMessage(ip);
+    });
 
-    this.observer.onThresholdReached(this.threshold, async () => {
-      const startToStopTransitionHandler = new DelayedTransitionHandler(
-        this.delayStartupMs
-      )
-      startToStopTransitionHandler.onTransitionScheduled(async () => {
-        await this.messageHandler.sendStartScheduledMessage(this.delayStartupMs)
-      })
+    this.observer.onThresholdReached(
+      this.startUpThreshold,
+      this.thresholdReachedHandler
+    );
 
-      startToStopTransitionHandler.onTransitionFinished(async() => {
-        await this.messageHandler.sendStartingMessage()
-        //await vmHandler.start();
-      })
-
-      if(startToStopTransitionHandler.is(State.STATUS_A)) {
-        startToStopTransitionHandler.scheduleTransition();
-      }
-    })
-
-    this.observer.onThresholdLeft(this.threshold, async () => {
-      const stopToStartTransitionHandler = new DelayedTransitionHandler(
-        this.delayShutdownMs,
-      );
-
-      stopToStartTransitionHandler.onTransitionScheduled(async () => {
-        await this.messageHandler.sendStopScheduledMessage(this.delayShutdownMs)
-      })
-
-      stopToStartTransitionHandler.onTransitionFinished(async() => {
-        await this.messageHandler.sendStopMessage()
-        //await vmHandler.stop();
-      })
-      
-      if(stopToStartTransitionHandler.is(State.STATUS_A)) {
-        stopToStartTransitionHandler.scheduleTransition();
-      }
-    })
+    this.observer.onThresholdLeft(
+      this.shutdownUpThreshold,
+      this.thresholdLeftHandler
+    );
   }
 }
